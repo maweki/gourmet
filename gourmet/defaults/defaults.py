b186d3b1bfb1a2cc500a202be3389d38192f9099
Mario Wenzel <maweki@gmail.com>
2020-05-14 23:30:07 +0200

    Rewrite Language Modules to language classes

--- gourmet/defaults/defaults.py
+++ gourmet/defaults/defaults.py
@@ -1,6 +1,8 @@
 import locale, os
+from typing import Optional
+from .abstractLang import AbstractLanguage
 deflang = 'en'
-lang = None
+lang: AbstractLanguage
 
 if os.name == 'posix':
     try:
@@ -19,14 +21,16 @@ elif os.name == 'nt':
 
 if loc:
     try:
-        lang = __import__('defaults_%s'%loc,globals(),locals(), level=1)
+        lang = __import__('defaults_%s'%loc,globals(),locals(), level=1).Language
     except ImportError:
         try:
-            lang = __import__('defaults_%s'%loc[0:2],globals(),locals(), level=1)
+            lang = __import__('defaults_%s'%loc[0:2],globals(),locals(), level=1).Language
         except ImportError:
-            lang = __import__('defaults_%s'%deflang,globals(),locals(), level=1)
+            lang = __import__('defaults_%s'%deflang,globals(),locals(), level=1).Language
 
-if not lang: lang = __import__('defaults_%s'%deflang,globals(),locals())
+if not lang:
+    lang = __import__('defaults_%s'%deflang,globals(),locals()).Language
+
 
 # The next item is used to allow us to know some things about handling the language
 try:
@@ -76,7 +80,7 @@ unit_rounding_guide = {
     'c.':0.125,
     }
 
-if hasattr(lang,'unit_rounding_guide'):
+if hasattr(lang,'unit_rounding_guide') and lang.unit_rounding_guide:
     unit_rounding_guide.update(lang.unit_rounding_guide)
 
 lang.unit_rounding_guide = unit_rounding_guide
